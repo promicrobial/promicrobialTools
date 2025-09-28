9fe008ddf818fb641a5b81c6c18088746c3b6bf8
Nathaniel Cole <nc564@cornell.edu>
2025-09-27 23:37:11 -0400

    Revert "docs: spelling and remove redundant qmd section"
    Accidentally included vignette not ready for commit yet
    
    This reverts commit b876f037f1ee527fcd58053afcb2935404a5a1b8.

--- R/emm-zi.R
+++ R/emm-zi.R
@@ -139,12 +139,22 @@ or_to_cohens_d <- function(OR, P0) {
 #' results <- summarise_zi_contrasts(emm_zi, contrasts_zi, zi_model)
 #'
 #' # Format for publication
-#' results_table <- summarise_zi_contrasts(emm_zi, contrasts_zi, zi_model, format = "kable")
+#' results_table <- summarise_zi_contrasts(emm_zi, contrasts_zi, zi_model,
+#'                                        format = "kable")
 #' }
+#'
+#' @section Usage in Quarto/RMarkdown:
+#' ```{r}
+#' #| label: zi-effects
+#' #| tbl-cap: "Zero-Inflation Component Effect Sizes"
+#'
+#' summarise_zi_contrasts(emm_zi, contrasts_zi, model, 
+#'                       format = "kable")
+#' ```
 #'
 #' @importFrom dplyr mutate case_when
 #' @importFrom stats confint qnorm
-#' @importFrom emmeans emmeans contrast
+#' @importFrom emmeans emmeans pairs
 #' @export
 summarise_zi_contrasts <- function(emm_zi, 
                                   contrasts_zi, 
